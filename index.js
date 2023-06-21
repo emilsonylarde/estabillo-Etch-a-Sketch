9f14080f8ccd47453a1832b4338dda149c4fe71e
Emilson Ylarde <emilsonjylarde@gmail.com>
2023-06-21 17:36:24 +0800

    added coloring the square funtion

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 //creates a default board of 16x16
 function populateBoard(size) {
   let board = document.querySelector(".board");
+  let squares = board.querySelectorAll("div");
+  squares.forEach((div) => div.remove());
   board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-  for (let i = 0; i < 256; i++) {
+  let amount = size * size;
+  for (let i = 0; i < amount; i++) {
     let square = document.createElement("div");
-    square.style.backgroundColor = "blue";
+    square.addEventListener("mouseover", colorSquare);
+    square.style.backgroundColor = "white";
     board.insertAdjacentElement("beforeend", square);
   }
 }
@@ -15,5 +19,14 @@ populateBoard(16);
 
 //a function to change board size
 function changeSize(input) {
-  populateBoard(input);
+  if (input >= 2 && input <= 100) {
+    populateBoard(input);
+  } else {
+    console.log("Too many squares!");
+  }
+}
+
+//a function to color square 
+function colorSquare() {
+  this.style.backgroundColor = "black";
 }
